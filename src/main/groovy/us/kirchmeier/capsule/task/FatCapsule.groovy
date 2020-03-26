9460997cbb58af573841b0b980f1ab21063a6643
Chris Mayes <chris.mayes@proquest.com>
2020-03-26 11:15:57 -0600

    PQ-67354: Needed to use the default classpath when the runtime classpath is empty

--- src/main/groovy/us/kirchmeier/capsule/task/FatCapsule.groovy
+++ src/main/groovy/us/kirchmeier/capsule/task/FatCapsule.groovy
@@ -3,6 +3,15 @@ package us.kirchmeier.capsule.task
 class FatCapsule extends Capsule {
   FatCapsule() {
     applicationSource = project.tasks.findByName('jar')
-    embedConfiguration = project.configurations.runtime
+      def runtimeConfig = project.configurations.runtime
+      getLogger().info("Runtime name: ${runtimeConfig.name}")
+      getLogger().info("Runtime size: ${runtimeConfig.files.size()}")
+      if (runtimeConfig.files.size() < 2) {
+          getLogger().info("Need to find new config")
+          embedConfiguration = project.configurations.default
+      } else {
+          getLogger().info("Using original runtime config")
+          embedConfiguration = project.configurations.runtime
+      }
   }
 }
